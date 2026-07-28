e003eb6fb3c93f13bcef3fc29ae03f852161b33d
Siddhant Hardik <hardiksiddhant@gmail.com>
2026-07-28 21:09:02 +0530

    feat: install Google Tag Manager (GTM-KJLKVHMQ) and Google Analytics (G-Z31NGSX2WV) in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import Providers from "@/components/providers";
 import { PwaInstallBanner } from "@/components/pwa-install-banner";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Gyrex - Practice Growth Platform",
@@ -35,8 +36,43 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+
+        {/* Google Tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z31NGSX2WV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z31NGSX2WV');
+          `}
+        </Script>
+
+        {/* Google Tag Manager Container */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KJLKVHMQ');
+          `}
+        </Script>
       </head>
       <body className={cn("bg-gray-50 font-sans")}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KJLKVHMQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Providers>
           <PwaInstallBanner />
           {children}
